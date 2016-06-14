005392492eae8ebd728979d0db74ee5208c4e9ee
Yuyi <liyuyi828@gmail.com>
2016-06-14 16:28:45 -0700

    implement all class of bee

--- src/Bee.js
+++ src/Bee.js
@@ -6,7 +6,8 @@ var Bee = function() {
 	this.age = 5; 
 	this.color = 'yellow';
 	this.job = 'keep on growing'; 
-
+ 
 };
 
-Bee.prototype = Object.create(Grub.prototype); 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee; 
--- src/Grub.js
+++ src/Grub.js
@@ -2,13 +2,13 @@ var Grub = function() {
 	//Object.call(this);
 	this.age = 0;
 	this.color = 'pink'; 
-	this.food = 'jelly'; 
-	this.eat = function() {
-	}; 
+	this.food = 'jelly';  
 };
 
-Grub.prototype = Object.create(Object.prototype);
-// Grub.prototype.somethingNew = function() {
-// }
-Grub.prototype.constructor = Grub; 
+// Grub.prototype = Object.create(Object.prototype);
+// // Grub.prototype.somethingNew = function() {
+// // }
+Grub.prototype.constructor = Grub;
+Grub.prototype.eat = function() {
 
+}; 
